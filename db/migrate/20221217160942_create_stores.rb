9a50215a2e1b1f5dc5fe9618f87cf550fdff3d88
ztchia <zhengtom93@outlook.com>
2022-12-18 00:19:59 +0800

    Add store

--- db/migrate/20221217160942_create_stores.rb
+++ db/migrate/20221217160942_create_stores.rb
@@ -4,7 +4,6 @@ class CreateStores < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :type
       t.string :location
-      t.references :menu, null: false, foreign_key: true
 
       t.timestamps
     end
