32cb7f4c69f99ff135b0db184fa55bb9ed0630c0
ztchia <zhengtom93@outlook.com>
2022-12-18 02:24:47 +0800

    Schema update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,9 +6,16 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     render json: {
-      status: {code: 200, message: 'Logged in sucessfully.'},
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
+             status: {
+               code: 200,
+               message: "Logged in sucessfully."
+             },
+             data:
+               UserSerializer.new(resource).serializable_hash[:data][
+                 :attributes
+               ]
+           },
+           status: :ok
   end
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,18 @@
 Rails.application.routes.draw do
-  get 'stores/create'
-  get 'stores/update'
-  get 'stores/show'
-  get 'stores/index'
-  get 'menus/create'
-  get 'menus/update'
-  get 'menus/show'
-  get 'menus/index'
-  get 'menus/destroy'
-  get 'create/update'
-  get 'create/show'
-  get 'create/index'
-  get 'create/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users,
+             path: "",
+             path_names: {
+               sign_in: "login",
+               sign_out: "logout",
+               registration: "signup"
+             },
+             controllers: {
+               sessions: "users/sessions",
+               registrations: "users/registrations"
+             }
 
-  get '/current_user', to: 'current_user#index'
+  resources :stores, only: %i[create show update index]
+  resources :menus, only: %i[create show update index]
+  resource :current_user, only: :show
 end
