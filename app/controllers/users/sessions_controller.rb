223132d95a7eb43dd72ec299d59cb9ecb555aa5b
ztchia <zhengtom93@outlook.com>
2022-12-17 20:16:53 +0800

    Add devise

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  # before_action :configure_sign_in_params, only: [:create]
+
+  def respond_with(resource, _opts = {})
+    render json: {
+      status: {code: 200, message: 'Logged in sucessfully.'},
+      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }, status: :ok
+  end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
